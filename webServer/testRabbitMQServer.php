093eb2928790eda6e219cfb17fea3819d9025e24
bsc26 <bsc26@njit.edu>
2022-10-20 12:16:25 -0400

    Edited Loginpage and have Register redirects and added Registering new user functionality

--- webServer/testRabbitMQServer.php
+++ webServer/testRabbitMQServer.php
@@ -32,6 +32,35 @@ $response = $mydb->query($query);
         else    {return true;}
 }
 
+
+function doRegister($username,$password,$email)
+{
+	        //Osama Login In Here
+$mydb = new mysqli('127.0.0.1','osama','password1','UFC');
+
+if ($mydb->errno != 0)
+{
+        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+        exit(0);
+}
+
+//echo "successfully connected to database".PHP_EOL;
+
+$query = "insert into users (username, password, email) values ('$username', '$password', '$email')";
+
+$response = $mydb->query($query);
+        if ($mydb->errno != 0)
+        {
+                echo "failed to execute query:".PHP_EOL;
+                echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+                return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
 function requestProcessor($request)
 {
   echo "received request".PHP_EOL;
@@ -53,7 +82,19 @@ function requestProcessor($request)
 		 echo "Invalid Login".PHP_EOL;
 		 return array("returnCode" => '1', 'message'=>"Invalid Login");
 	  }
-    case "validate_session":
+  case "Register":
+	if (doRegister($request['username'],$request['password',$request['email']))
+	{
+		echo "Succesful Register".PHP_EOL;
+		return array("returnCode" => '0', 'message'=>"Successful Register");
+	}
+	else
+	{
+		echo "Register Failed".PHP_EOL;
+		return array("returnCode" => '1', 'message'=>"Register Failed");
+	}
+
+  case "validate_session":
       return doValidate($request['sessionId']);
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");}
