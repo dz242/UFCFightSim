8d9e0aafdf492e21dc95bba033aafcd7f15b305a
bsc26 <bsc26@njit.edu>
2022-12-18 22:03:07 -0500

    more shop fixes

--- webServer/shoppage.php
+++ webServer/shoppage.php
@@ -39,13 +39,13 @@ echo $argv[0]." END".PHP_EOL;
 <html>
 <body>
 	<!--Echo User Currency Form action should Return to same page-->
-	<h1>"Your Currency: "<?php print_r($response[0][8]); ?></h1>
-	<h2>"Each Fighter is 100 Currency"</h2>
+	<h1>Your Currency: <?php print_r($response[0][7]); ?></h1>
+	<h2>Each Fighter is 100 Currency</h2>
 	<form action="shopsubmit.php" id="submission" method="post">
 
 		<select name="shop" id="shop">
 <?php
-	$fighterList = array_slice($response, 1)
+	$fighterList = array_slice($response, 1);
         foreach($fighterList as $fighter)
         {
             echo "<option value = '$fighter[1]'>$fighter[6]</option>";  
